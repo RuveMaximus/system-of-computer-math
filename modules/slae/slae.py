c777119b6689f9688fed69b9bbe58391a3b424eb
Никита Максимов <gameboss287@gmail.com>
2022-12-17 21:11:06 +0800

    Поиск обратной матрицы

--- modules/slae/slae.py
+++ modules/slae/slae.py
@@ -1,9 +1,11 @@
 from modules.matrices import matrix
 
+
 def count_row_position(row: list):
     pos = 0
     while row[pos] == 0: pos+=1
     return pos 
+
 
 def refresh_matrix(m: list):
     _matrix = [None]*len(m)
@@ -14,6 +16,7 @@ def refresh_matrix(m: list):
 
         _matrix[pos] = row
     return _matrix
+
 
 def is_solution(m: list) -> bool:
     for i in range(len(m)):
@@ -37,17 +40,27 @@ def left_complete(m):
                 return False
     return True
 
+
 def right_complete(m):
     for i in range(len(m)):
         for j in range(i+1, len(m)):
             if m[i][j] != 0:
                 return False
     return True
+    
+
+def attach_unit_matrix(m):
+    for i in range(len(m)):
+        row = []
+        for j in range(i): row.append(0)
+        row.append(1)
+        for j in range(len(m)-i-1): row.append(0)
+        m = add_column(m, row)
+    
+    return m
 
 
-def solve(m: list, result_column: list):
-    m = add_column(m, result_column)
-
+def inverse_matrix(m):
     while not left_complete(m):
         for i in range(len(m)):
             if m[i][i] == 0: m = refresh_matrix(m)
@@ -62,6 +75,22 @@ def solve(m: list, result_column: list):
             for j in range(i+1, len(m)):
                 m = matrix.sum_matrix_rows(m, [i, j], [1, -m[i][j]])
 
-    print(m, is_solution(m))
+    return m
+
+
+def solve(m: list, result_column: list):
+    m = add_column(m, result_column)
+    m = inverse_matrix(m)
 
     return [round(row[-1], 2) for row in m]
+
+
+def solve_by_inverse_matrix(m: list, result_column: list):
+    m = attach_unit_matrix(m)
+    m = inverse_matrix(m)
+
+    m = [row[len(result_column):] for row in m]
+    result_column = [[row]*len(result_column) for row in result_column]
+    m = matrix.multiply(m, result_column)
+    
+    return [row[0] for row in m]
--- modules/slae/test_slae.py
+++ modules/slae/test_slae.py
@@ -14,6 +14,14 @@ def test_is_solution():
     assert slae.is_solution([[1, 0, 2.5], [0, 1, -1]])
     assert not slae.is_solution([[1, 2, 2.5], [0, 1, -1]])
 
+def test_inverse_matrix(): 
+    m = slae.attach_unit_matrix([[1, 2], [3, 4]])
+    m = slae.inverse_matrix(m)
+    assert [row[len(m)//2+1:] for row in m] == [[-2, 1], [1.5, -0.5]]
+
 def test_solve():
     assert slae.solve([[2, 3], [4, 3]], [2, 7]) == [2.5, -1.0]
     assert slae.solve([[-1, 2, 6], [3, -6, 0], [1, 0, 6]], [15, -9, 5]) == [-7, -2, 2]
+
+def test_solve_by_inverse_matrix(): 
+    assert slae.solve_by_inverse_matrix([[1, 2], [3, 4]], [6, 8]) == [-4, 5]
