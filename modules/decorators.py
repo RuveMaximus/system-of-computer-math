a9f54dbbd90211deac2d99d863751b587488e64a
Nikita <n.maksimov@ispsystem.com>
2022-12-22 13:10:46 +0800

    Улучшение декоратора

--- modules/decorators.py
+++ modules/decorators.py
@@ -3,6 +3,6 @@ from copy import deepcopy
 
 def args_copy_need(func: callable):
     def wrapper(*args, **kwargs):
-        args = list(map(deepcopy, args))
+        args = map(deepcopy, args)
         return func(*args, **kwargs)
     return wrapper
